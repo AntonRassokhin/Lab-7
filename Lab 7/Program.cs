8f0dd877a28392e13149a5566a876b9abca05717
Rassokhin Anton <antonrassohin@gmail.com>
2021-11-23 23:06:21 +0300

    Добавьте файлы проекта.

---
+++ Lab 7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int V, S;
+            Console.Write("Введите длину ребра куба: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            getKotlet(a, out V, out S);
+
+            Console.WriteLine("Объем - {0:0,0}", V);
+            Console.WriteLine("Площадь поверхности - {0:0,0}", S);
+            Console.ReadKey();
+        }
+
+        static void getKotlet(int a, out int V, out int S)
+        {
+            V = a * a * a;
+            S = a * a * 6;
+        }
+
+    }
+}
---
+++ Задание 2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double S1, S2;
+            Console.WriteLine("Введите 3 стороны первого треугольника:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+            calcul(a, b, c, out S1);
+
+            Console.WriteLine("Введите 3 стороны второго треугольника:");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            calcul(a, b, c, out S2);
+
+            if (S1 == S2)
+            {
+                Console.WriteLine("Площади треугольников равны");
+            }
+            else
+            {
+                if (S1 > S2)
+                {
+                    Console.WriteLine("Площадь первого треугольника больше");
+                }
+                else
+                {
+                    Console.WriteLine("Площадь второго треугольника больше");
+                }
+
+            }
+            Console.ReadKey();
+
+        }
+        static void calcul(double a, double b, double c, out double S)
+        {
+            double pP = (a + b + c) / 2;
+            S = Math.Sqrt(pP * (pP - a) * (pP - b) * (pP - c));
+            Console.WriteLine("Полупериметр -{0:F2} , площадь -{1:F2}", pP, S); ///для проверки
+        }
+
+    }
+}
